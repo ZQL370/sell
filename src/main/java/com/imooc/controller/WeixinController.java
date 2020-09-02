0105d2c7953f428c61024b44c3725264d15ef35c
imooc <1575707417@qq.com>
2020-09-02 10:53:41 +0800

    微信授权

---
+++ src/main/java/com/imooc/controller/WeixinController.java
@@ -0,0 +1,28 @@
+package com.imooc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 用于微信授权的controller接口
+ */
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+        log.info("进入auth方法。。。");
+        log.info("code={}", code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx89940ca2633f607a&secret=31bc7d974554f7f11fc630de4e023119&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
+    }
+}
